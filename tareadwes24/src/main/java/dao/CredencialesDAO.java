6f45b8fb4bb6eeec723e461615b94faa2273f65e
OscarMateosRamos <dsi85944@educastur.es>
2024-11-04 13:34:55 +0100

    Cambios realizados en CredencialesDAO

--- tareadwes24/src/main/java/dao/CredencialesDAO.java
+++ tareadwes24/src/main/java/dao/CredencialesDAO.java
@@ -5,127 +5,55 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
 
 import modelo.Credenciales;
 import utils.ConexBD;
 
 public class CredencialesDAO implements OperacionesCrud<Credenciales> {
 
-	
-	static Connection conex = null;
-
-	public CredencialesDAO() {
-		super();
-		conex = ConexBD.realizaConexion();
-		if (conex == null) {
-			System.out.println("Error al conectar a la BD");
-		}
-	}
-
+	Connection conex;
+	ResultSet rs;
+	Statement st;
 	PreparedStatement ps;
 
-	@Override
-	public void insertar(Credenciales c) {
-		String sql = "INSERT INTO credenciales(id,usuario,password) values (?,?,?)";
-		ResultSet rs;
-		Statement st;
-		PreparedStatement ps;
-
-		try {
-			if (conex == null) {
-				conex = ConexBD.realizaConexion();
-				ps = conex.prepareStatement(sql);
-
-				ps.setLong(1, c.getId());
-				ps.setString(2, c.getUsuario());
-				ps.setString(3, c.getPassword());
-
-				ps.executeUpdate();
-				System.out.println("Credenciales insertadas correctamente");
-
-			}
-			conex.close();
-		} catch (SQLException e) {
-			System.out.println("Error al insertar en planta");
+	public CredencialesDAO(Connection conex) {
+		if (this.conex == null) {
+			this.conex = conex;
 		}
 	}
 
 	@Override
-	public void borrar(Credenciales c) {
+	public long insertar(Credenciales c) {
+
 		try {
-			if (conex == null) {
-				conex = ConexBD.realizaConexion();
-				String sql = ("DELETE FROM credenciales WHERE id =?");
-				Statement stmt = conex.createStatement();
 
-				ps.setLong(1, c.getId());
+			String sql = "INSERT INTO credenciales(id,usuario,password) values (?,?,?)";
+			ps = conex.prepareStatement(sql);
 
-				int filasborradas = ps.executeUpdate();
-				if (filasborradas > 0) {
-					System.out.println("Credenciales borradas con exito");
-				} else {
-					System.out.println("No se encontró una credencial con ese id.");
-				}
-				conex.close();
-			}
-			conex.close();
+			ps.setLong(1, c.getId());
+			ps.setString(2, c.getUsuario());
+			ps.setString(3, c.getPassword());
+
+			return ps.executeUpdate();
+
 		} catch (SQLException e) {
-			System.out.println("Error al borrar en credenciales" + e.getMessage());
+			System.out.println("Error al insertar en Credenciales");
 		}
+		return 0;
 
 	}
 
 	@Override
-	public void modificar(Credenciales c) {
-		try {
-			if (conex == null) {
-				conex = ConexBD.realizaConexion();
-			}
-			String sql = "UPDATE credenciale SET usuario = ?, password = ? WHERE id = ?";
-			Statement stmt = conex.createStatement();
-
-			ps.setString(1, c.getUsuario());
-			ps.setString(2, c.getPassword());
-			ps.setLong(3, c.getId());
-
-			int filasactualizadas = ps.executeUpdate();
-			if (filasactualizadas > 0) {
-				System.out.println("Modificado con éxito");
-			} else {
-				System.out.println("No se encontró una Credencial con el Id especificado.");
-			}
-			stmt.executeUpdate(sql);
-
-			conex.close();
-		} catch (SQLException e) {
-			System.out.println("Error al modificar en Credenciales" + e.getMessage());
-		}
-
+	public boolean modificar(Credenciales elemento) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public void  Collection<Credenciales> vertodas(Credenciales c) {
-
-		try {
-			if (this.conex == null || this.conex.isClosed()) {
-				this.conex = ConexBD.realizaConexion();
-			}
-			String sql = "SELECT * FROM credenciales";
-			Statement stmt = conex.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-
-			while (rs.next()) {
-				long id = rs.getLong("Id");
-				String usuario = rs.getString("usuario");
-				String password = rs.getString("email");
-
-				System.out.println("Id: " + id + ", Usuario: " + usuario + ", Password: " + password);
-			}
-			conex.close();
-		} catch (SQLException e) {
-			System.out.println("Error al listar en credenciales" + e.getMessage());
-		}
-
+	public Collection<Credenciales> verTodas() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
