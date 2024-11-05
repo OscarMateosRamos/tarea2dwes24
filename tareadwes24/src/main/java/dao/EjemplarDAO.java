8277bc75e1789561a15669ee501ad1e3ca8e8382
Osmara <dsi85944@educastur.es>
2024-11-05 19:58:49 +0100

    Cambios realizados en EjemplarDAO

--- tareadwes24/src/main/java/dao/EjemplarDAO.java
+++ tareadwes24/src/main/java/dao/EjemplarDAO.java
@@ -5,14 +5,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import modelo.Ejemplar;
+import modelo.Mensaje;
+import modelo.Planta;
 import utils.ConexBD;
 
 public class EjemplarDAO implements OperacionesCrud<Ejemplar> {
 
-	
 	Connection conex;
 	ResultSet rs;
 	Statement st;
@@ -28,11 +30,12 @@ public class EjemplarDAO implements OperacionesCrud<Ejemplar> {
 	public long insertar(Ejemplar ej) {
 		try {
 
-			String sql = "INSERT INTO ejemplar(id,nombre) values (?,?)";
+			String sql = "INSERT INTO ejemplar(id,nombre,idPlanta) values (?,?,?)";
 			ps = conex.prepareStatement(sql);
 
 			ps.setLong(1, ej.getId());
 			ps.setString(2, ej.getNombre());
+			ps.setString(3, ej.getIdPlanta());
 
 			return ps.executeUpdate();
 
@@ -43,38 +46,90 @@ public class EjemplarDAO implements OperacionesCrud<Ejemplar> {
 	}
 
 	@Override
-	public boolean eliminar(Ejemplar ej) {
+	public boolean modificar(Ejemplar ej) {
 		try {
 
-			String sql = "DELETE FROM persona WHERE id=?";
-			ps = conex.prepareStatement(sql);
+			String sql = "UPDATE ejemplares SET nombre = ? WHERE id = ?";
+			PreparedStatement ps = conex.prepareStatement(sql);
 
 			ps.setLong(1, ej.getId());
 
+			return ps.executeUpdate() > 0;
 		} catch (SQLException e) {
-			System.out.println("Error al borrar en ejemplar" + e.getMessage());
-			e.printStackTrace();
+			System.out.println("Error al modificar en planta" + e.getMessage());
 		}
-		return false;
-
-	}
-
-	@Override
-	public boolean modificar(Ejemplar elemento) {
-		
 		return false;
 	}
 
 	@Override
 	public Collection<Ejemplar> verTodas() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM ejemplares";
+		ArrayList<Ejemplar> ejemplares = new ArrayList<>();
+
+		try {
+			if (this.conex == null || this.conex.isClosed()) {
+				this.conex = ConexBD.realizaConexion();
+			}
+
+			PreparedStatement ps = conex.prepareStatement(sql);
+
+			ResultSet res = ps.executeQuery();
+			while (res.next()) {
+
+				Ejemplar ejemplar = new Ejemplar(res.getLong("id"), res.getString("nombre"), res.getString("idPlanta"));
+
+				ejemplares.add(ejemplar);
+
+			}
+			conex.close();
+
+		} catch (SQLException e) {
+			System.out.println("Error al ver los ejemplares" + e.getMessage());
+		}
+
+		return ejemplares;
 	}
 
 	@Override
-	public Ejemplar buscarPorID(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Ejemplar buscarPorID(long identificador) {
+
+		String sql = "SELECT * FROM ejemplares WHERE codigo = ?";
+		Ejemplar ej = null;
+		try {
+			PreparedStatement ps = conex.prepareStatement(sql);
+			ps.setLong(1, identificador);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+			}
+		} catch (SQLException e) {
+			System.out.println("Se ha producido una SQLException: " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Se ha producido una Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return ej;
+
+	}
+
+	@Override
+	public boolean eliminar(Ejemplar ej) {
+		try {
+
+			String sql = "DELETE FROM ejemplares WHERE codigo = ?";
+			PreparedStatement ps = conex.prepareStatement(sql);
+
+			ps.setLong(1, ej.getId());
+
+			return ps.executeUpdate() > 0;
+		} catch (SQLException e) {
+			System.out.println("Error al eliminar en ejemplar" + e.getMessage());
+		}
+
+		return false;
 	}
 
 }
