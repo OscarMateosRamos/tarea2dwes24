b1a2fac1b0554df079f7b119f2e5c1e602e21b61
Osmara <dsi85944@educastur.es>
2024-11-06 21:34:48 +0100

    Cambios realizados en MensajeDAo

--- tareadwes24/src/main/java/dao/MensajeDAO.java
+++ tareadwes24/src/main/java/dao/MensajeDAO.java
@@ -4,106 +4,95 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 import modelo.Mensaje;
+
 import utils.ConexBD;
 
 public class MensajeDAO implements OperacionesCrud<Mensaje> {
 
-	
-	static Connection conex = null;
+	Connection conex;
+	private PreparedStatement ps;
+	private ResultSet rs;
 
-	public MensajeDAO() {
-		super();
-		conex = ConexBD.realizaConexion();
-		if (conex == null) {
-			System.out.println("Error al conectar a la BD");
+	public MensajeDAO(Connection conex) {
+
+		if (this.conex == null) {
+			this.conex = conex;
 		}
 	}
 
-	PreparedStatement ps;
-
 	@Override
-	public void insertar(Mensaje m) {
-		String sql = "INSERT INTO mensaje(id,fechahora,mensaje) values (?,?,?)";
-		ResultSet rs;
-		Statement st;
-		PreparedStatement ps;
+	public long insertar(Mensaje m) {
 
 		try {
-			if (conex == null) {
-				conex = ConexBD.realizaConexion();
-				ps = conex.prepareStatement(sql);
 
-				ps.setLong(1, m.getId());
-				/* ps.set(2, m.getFechahora()); */
-				ps.setString(3, m.getMensaje());
+			String sql = "INSERT INTO mensajes(id,fechahora,mensaje,idEjemplar,idPersona) values (?,?,?,?,?)";
+			ps = conex.prepareStatement(sql);
 
-				ps.executeUpdate();
-				System.out.println("Mendaje insertado correctamente");
+			ps.setLong(1, m.getId());
+			ps.setTimestamp(2, Timestamp.valueOf(m.getFechahora()));
+			ps.setString(3, m.getMensaje());
+			ps.setLong(4, m.getIdEjemplar());
+			ps.setLong(5, m.getIdPersona());
 
-			}
-			conex.close();
+			return ps.executeUpdate();
+
 		} catch (SQLException e) {
-			System.out.println("Error al insertar en mensaje");
+			System.out.println("Error al insertar en mensaje" + e.getMessage());
 		}
+
+		return 0;
 	}
 
 	@Override
-	public void borrar(Mensaje m) {
+	public boolean eliminar(Mensaje elemento) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificar(Mensaje elemento) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Mensaje> verTodas() {
+		String sql = "SELECT * FROM mensajes";
+		ArrayList<Mensaje> mensajes = new ArrayList<>();
+
 		try {
-			if (conex == null) {
-				conex = ConexBD.realizaConexion();
-				String sql = ("DELETE FROM mensaje WHERE id =?");
-				Statement stmt = conex.createStatement();
+			if (this.conex == null || this.conex.isClosed()) {
+				this.conex = ConexBD.realizaConexion();
+			}
 
-				ps.setLong(1, m.getId());
+			PreparedStatement ps = conex.prepareStatement(sql);
 
-				int filasborradas = ps.executeUpdate();
-				if (filasborradas > 0) {
-					System.out.println("Mensaje  borrado con éxito.");
-				} else {
-					System.out.println("No se encontró un mensaje con ese id.");
-				}
-				conex.close();
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Mensaje mensaje = new Mensaje(rs.getLong("id"), rs.getTimestamp("fechahora").toLocalDateTime(),
+						rs.getString("mensaje"),rs.getLong("idEjemplar"),rs.getLong("idPersona"));
+				mensajes.add(mensaje);
 			}
 			conex.close();
+
 		} catch (SQLException e) {
-			System.out.println("Error al borrar en mensaje" + e.getMessage());
+			System.out.println("Error al ver los mensajes" + e.getMessage());
 		}
-	}
 
-	@Override
-	public void modificar(Mensaje m) {
-		try {
-			if (conex == null) {
-				conex = ConexBD.realizaConexion();
-			}
-			String sql = "UPDATE mensaje SET fechahora = ?,mensaje=? WHERE  id = ?";
-			Statement stmt = conex.createStatement();
-
-			/*
-			 * ps.setString(1, ej.getNombre()); ps.setLong(2, ej.getId());
-			 */
-
-			int filasactualizadas = ps.executeUpdate();
-			if (filasactualizadas > 0) {
-				System.out.println("Modificado con éxito");
-			} else {
-				System.out.println("No se encontró el mensaje con el Id especificado.");
-			}
-			stmt.executeUpdate(sql);
-			conex.close();
-		} catch (SQLException e) {
-			System.out.println("Error al modificar el mensaje" + e.getMessage());
-		}
+		return mensajes;
 
 	}
 
 	@Override
-	public void vertodas(Mensaje m) {
-
+	public Mensaje buscarPorID(long id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
