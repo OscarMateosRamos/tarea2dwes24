fee1d07f66ec6952b0380ef9fa924306f69dfe4a
Osmara <dsi85944@educastur.es>
2025-04-21 18:33:28 +0200

    Cambios realizados en el proyecto

--- tareadwes24/src/main/java/main/Principal.java
+++ tareadwes24/src/main/java/main/Principal.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import control.Controlador;
+import control.ServiciosPlanta;
 import vista.ViveroFachadaAdmin;
 import vista.ViveroFachadaInvitado;
 import vista.ViveroFachadaPersonal;
@@ -17,137 +18,152 @@ public class Principal {
 
 	private static Controlador controlador = Controlador.getServicios();
 
-	ViveroFachadaInvitado portalInvitado = ViveroFachadaInvitado.getPortal();
+	private static ViveroFachadaInvitado portalInvitado;
+
+	// Connection con = ConexBD.realizaConexion();
+
+	Controlador factoriaServicios = Controlador.getServicios();
+
+	ServiciosPlanta plantaServ = factoriaServicios.getServiciosPlanta();
 
 	public static void menuLogin() {
 		int opcion = 0;
-
 		Scanner sc = new Scanner(System.in);
+
 		do {
-			System.out.println("---VIENVENIDO AL VIVERO ---");
-			System.out.println("1.  ENTRAR COMO INVITADO");
-			System.out.println("2.  LOGIN (PERSONAL O ADMIN)");
-			System.out.println("3.  SALIR");
+			System.out.println("\n--- BIENVENIDO AL VIVERO ---");
+			System.out.println("1. ENTRAR COMO INVITADO");
+			System.out.println("2. LOGIN (PERSONAL O ADMIN)");
+			System.out.println("3. SALIR");
+			System.out.print("Selecciona una opción: ");
+
 			try {
 				opcion = sc.nextInt();
-				if (opcion < 1 || opcion > 4) {
-					System.out.println("Opción incorrecta.");
-					continue;
-				}
-				switch (opcion) {
-				case 1:
-					ViveroFachadaInvitado portalInvitado = ViveroFachadaInvitado.getPortal();
-					portalInvitado.getPortal().mostrarMenuPrincipal();
-
-					break;
-				case 2:
-
-					sc = new Scanner(System.in);
-					System.out.println(" Introduce Usuario");
-					String usuario = sc.nextLine();
-					System.out.println("Introduce Contraseña");
-					String password = sc.nextLine();
-
-					boolean userOk = Controlador.getServicios().getServiciosCredenciales().validarCredencial(usuario,
-							password);
-					if (userOk) {
-						controlador.setUsername(usuario);
-						System.out.println("Usuario y password Correcto");
-
-						ViveroFachadaPersonal portalPersonal = ViveroFachadaPersonal.getPortal();
-						portalPersonal.getPortal().mostrarMenuPrincipal();
-					} else {
-						System.out.println("Usuario y password Erroneo");
-					}
-					break;
-
-				case 3:
-					System.out.println("-----------------");
-					System.out.println("Fin del programa has salido del vivero");
-					System.out.println("-----------------");
-					break;
-				}
-			} catch (InputMismatchException e) {
-				System.out.println("Tienes que introducir un numero....Repita el proceso");
 				sc.nextLine();
-				opcion = 0;
-			}
 
-		} while (opcion != 4);
-
-	}
-
-	//Refactorizar
-	public static void menuPersonalAdmin() {
-		int opcion = 0;
-
-		Scanner sc = new Scanner(System.in);
-		do {
-			System.out.println("--- INICIO DE SESION---");
-			System.out.println("1.  ENTRAR COMO PERSONAL");
-			System.out.println("2.  ENTRAR COMO ADMIN");
-			System.out.println("3.  Salir");
-			try {
-				opcion = sc.nextInt();
-				if (opcion < 1 || opcion > 4) {
-					System.out.println("Opción incorrecta.");
-					continue;
-				}
 				switch (opcion) {
-				case 1:
+				case 1: // Ver Plantas directamente
 
-					sc = new Scanner(System.in);
-					System.out.println(" Introduce Usuario");
-					String usuario = sc.nextLine();
-					System.out.println("Introduce Contraseña");
-					String password = sc.nextLine();
-
-					boolean userOk = Controlador.getServicios().getServiciosCredenciales().validarCredencial(usuario,
-							password);
-					if (userOk) {
-						controlador.setUsername(usuario);
-						System.out.println("Usuario y password Correcto");
-
-						ViveroFachadaPersonal portalPersonal = ViveroFachadaPersonal.getPortal();
-						portalPersonal.getPortal().mostrarMenuPrincipal();
-					} else {
-						System.out.println("Usuario y password Erroneo");
-					}
+					ViveroFachadaInvitado portalInvitado = ViveroFachadaInvitado.getPortal();
+					portalInvitado.mostrarPlantasInvitado();
 					break;
 
 				case 2:
-					sc = new Scanner(System.in);
-					System.out.println(" Introduce Usuario");
-					usuario = sc.nextLine();
-					System.out.println("Introduce Contraseña");
-					password = sc.nextLine();
+					System.out.print("Introduce Usuario: ");
+					String usuario = sc.nextLine();
+
+					System.out.print("Introduce Contraseña: ");
+					String password = sc.nextLine();
 
 					if (usuario.equals("admin") && password.equals("admin")) {
-						System.out.println("--Bienvenido Admin--");
+						System.out.println("-- Bienvenido Admin --");
 						controlador.setUsername("admin");
-
 						ViveroFachadaAdmin portalAdmin = ViveroFachadaAdmin.getPortal();
-
-						portalAdmin.getPortal().mostrarMenuPrincipal();
-
+						portalAdmin.mostrarMenuPrincipal();
 					} else {
-						System.out.println("Usuario y Password erroneas");
-					}
+						boolean userOk = Controlador.getServicios().getServiciosCredenciales()
+								.validarCredencial(usuario, password);
 
+						if (userOk) {
+							System.out.println("Usuario y contraseña correctos.");
+							controlador.setUsername(usuario);
+							ViveroFachadaPersonal portalPersonal = ViveroFachadaPersonal.getPortal();
+							portalPersonal.mostrarMenuPrincipal();
+						} else {
+							System.out.println("Usuario o contraseña incorrectos.");
+						}
+					}
 					break;
 
 				case 3:
-					menuLogin();
+					System.out.println("-----------------");
+					System.out.println("Fin del programa has salido del vivero.");
+					System.out.println("-----------------");
 					break;
+
+				default:
+					System.out.println("Opción incorrecta. Intenta de nuevo.");
 				}
+
 			} catch (InputMismatchException e) {
-				System.out.println("Tienes que introducir un numero....Repita el proceso");
+				System.out.println("Debes introducir un número. Intenta de nuevo.");
 				sc.nextLine();
-				opcion = 0;
 			}
 
-		} while (opcion != 4);
-
+		} while (opcion != 3);
 	}
+
+	// Refactorizar
+//	public static void menuPersonalAdmin() {
+//		int opcion = 0;
+//
+//		Scanner sc = new Scanner(System.in);
+//		do {
+//			System.out.println("--- INICIO DE SESION---");
+//			System.out.println("1.  ENTRAR COMO PERSONAL");
+//			System.out.println("2.  ENTRAR COMO ADMIN");
+//			System.out.println("3.  Salir");
+//			try {
+//				opcion = sc.nextInt();
+//				if (opcion < 1 || opcion > 4) {
+//					System.out.println("Opción incorrecta.");
+//					continue;
+//				}
+//				switch (opcion) {
+//				case 1:
+//
+//					sc = new Scanner(System.in);
+//					System.out.println(" Introduce Usuario");
+//					String usuario = sc.nextLine();
+//					System.out.println("Introduce Contraseña");
+//					String password = sc.nextLine();
+//
+//					boolean userOk = Controlador.getServicios().getServiciosCredenciales().validarCredencial(usuario,
+//							password);
+//					if (userOk) {
+//						controlador.setUsername(usuario);
+//						System.out.println("Usuario y password Correcto");
+//
+//						ViveroFachadaPersonal portalPersonal = ViveroFachadaPersonal.getPortal();
+//						portalPersonal.getPortal().mostrarMenuPrincipal();
+//					} else {
+//						System.out.println("Usuario y password Erroneo");
+//					}
+//					break;
+//
+//				case 2:
+//					sc = new Scanner(System.in);
+//					System.out.println(" Introduce Usuario");
+//					usuario = sc.nextLine();
+//					System.out.println("Introduce Contraseña");
+//					password = sc.nextLine();
+//
+//					if (usuario.equals("admin") && password.equals("admin")) {
+//						System.out.println("--Bienvenido Admin--");
+//						controlador.setUsername("admin");
+//
+//						ViveroFachadaAdmin portalAdmin = ViveroFachadaAdmin.getPortal();
+//
+//						portalAdmin.getPortal().mostrarMenuPrincipal();
+//
+//					} else {
+//						System.out.println("Usuario y Password erroneas");
+//					}
+//
+//					break;
+//
+//				case 3:
+//					menuLogin();
+//					break;
+//				}
+//			} catch (InputMismatchException e) {
+//				System.out.println("Tienes que introducir un numero....Repita el proceso");
+//				sc.nextLine();
+//				opcion = 0;
+//			}
+//
+//		} while (opcion != 4);
+//
+//	}
 
 }
--- tareadwes24/src/main/java/vista/ViveroFachadaInvitado.java
+++ tareadwes24/src/main/java/vista/ViveroFachadaInvitado.java
@@ -1,12 +1,10 @@
 package vista;
 
-import java.sql.Connection;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.List;
 
 import control.Controlador;
 import control.ServiciosPlanta;
-import utils.ConexBD;
+import modelo.Planta;
 
 public class ViveroFachadaInvitado {
 
@@ -16,7 +14,7 @@ public class ViveroFachadaInvitado {
 
 	}
 
-	//Connection con = ConexBD.realizaConexion();
+	// Connection con = ConexBD.realizaConexion();
 
 	Controlador factoriaServicios = Controlador.getServicios();
 
@@ -28,39 +26,42 @@ public class ViveroFachadaInvitado {
 		return portalInvitado;
 	}
 
-	public void mostrarMenuPrincipal() {
-		int opcion = 0;
-		Scanner sc = new Scanner(System.in);
-		do {
-			System.out.println("--Menu Invitado--:");
-			System.out.println("1.  Ver plantas.");
-			System.out.println("2.  Salir.");
+	public void mostrarPlantasInvitado() {
+		System.out.println("-- LISTADO DE PLANTAS --");
+		List<Planta> plantas = (List<Planta>) plantaServ.verTodas();
 
-			try {
-				opcion = sc.nextInt();
-				if (opcion < 1 || opcion > 2) {
-					System.out.println("Opción incorrecta.");
-					continue;
-				}
-				switch (opcion) {
-				case 1:
-					plantaServ.verTodas();
-					break;
-
-				case 2:
-					Controlador.getServicios().setUsername("invitado");
-					System.out.println("Salida del menu del menu invitado...");
-					System.out.println("");
-					break;
-				}
-
-			} catch (InputMismatchException e) {
-				System.out.println("Introduce un numero.... Repita el proceso");
-				sc.nextLine();
-				opcion = 0;
-			}
-
-		} while (opcion != 2);
+//		int opcion = 0;
+//		Scanner sc = new Scanner(System.in);
+//		do {
+//			System.out.println("--Menu Invitado--:");
+//			System.out.println("1.  Ver plantas.");
+//			System.out.println("2.  Salir.");
+//
+//			try {
+//				opcion = sc.nextInt();
+//				if (opcion < 1 || opcion > 2) {
+//					System.out.println("Opción incorrecta.");
+//					continue;
+//				}
+//				switch (opcion) {
+//				case 1:
+//					plantaServ.verTodas();
+//					break;
+//
+//				case 2:
+//					Controlador.getServicios().setUsername("invitado");
+//					System.out.println("Salida del menu del menu invitado...");
+//					System.out.println("");
+//					break;
+//				}
+//
+//			} catch (InputMismatchException e) {
+//				System.out.println("Introduce un numero.... Repita el proceso");
+//				sc.nextLine();
+//				opcion = 0;
+//			}
+//
+//		} while (opcion != 2);
 
 	}
 
